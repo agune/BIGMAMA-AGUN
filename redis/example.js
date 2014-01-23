126333017ae99a541725f22ab303ad543c18a7d4
agune <pdc222@nate.com>
2014-01-24 00:25:06 +0900

    add node_redis example

---
+++ redis/example.js
@@ -0,0 +1,27 @@
+/**
+*  This example code is node_redis study code.
+*  author agun
+*  see node_redis (https://github.com/mranney/node_redis)
+*  see BIGMAMA PROJECT (https://github.com/PROJECT-BIGMAMA/BIGMAMA-DOC)
+*/
+
+var redis = require("redis");
+var client = redis.createClient();
+
+client.on("error", function(err){
+	console.log("Error" + err);
+})
+
+
+client.set("string key", "string val", redis.print);
+client.hset("hash key", "hashtest 1", "some value", redis.print);
+client.hset(["hash key", "hashtest 2", "some other value"], redis.print);
+client.hkeys("hash key", function (err, replies) {
+	console.log(replies.length + " replies:");
+	replies.forEach(function (reply, i) {
+		console.log("    " + i + ": " + reply);
+	});
+	client.quit();
+ });
+ 
+ console.log("last line");
